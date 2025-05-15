f5ba3b1a251363ae3ec9e0c6dfb6e924ed27dcb4
farangiz14 <168212339+farangiz14@users.noreply.github.com>
2025-05-15 17:59:29 +0500

    Update python.py

--- lesson-1/homework/python.py
+++ lesson-1/homework/python.py
@@ -1,1 +1,40 @@
-
+#1
+side = float(input("Enter the side of the square: "))
+perimeter = 4 * side
+print("Perimeter:", perimeter)
+Perimeter: 8.0
+area = side ** 2
+print('Area:', area)
+Area: 4.0
+#2
+diameter = float(input('enter the diameter of the circle: '))
+enter the diameter of the circle: 3
+pi = 3.141592653589793
+circle_len = diameter * pi
+print("Circle length: ", circle_len)
+Circle length:  9.42477796076938
+#3
+a = float(input('enter value for a: '))
+enter value for a: 2
+b = float(input('enter value for b: '))
+enter value for b: 6
+mean = (a+b)/2
+print("Mean of a and b: ", mean)
+Mean of a and b:  4.0
+#4
+a = int(input('enter value for a: '))
+enter value for a: 2
+b = int(input('enter value for b: '))
+enter value for b: 4
+sum = a + b
+product = a * b
+a_square = a ** 2
+b_square = b ** 2
+print("sum: ", sum)
+sum:  6
+print("product: ", product)
+product:  8
+print("sqr_a: ", a_square)
+sqr_a:  4
+print("sqr_b: ", b_square)
+sqr_b:  16
